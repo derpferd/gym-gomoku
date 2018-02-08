00853e6e4c39dddd2711d9d91de2c07bdcf8e7f3
Jonathan Beaulieu <123.jonathan@gmail.com>
2018-02-08 11:51:34 -0600

    Some refactoring

--- gym_gomoku/envs/gomoku.py
+++ gym_gomoku/envs/gomoku.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 
 import numpy as np
 import gym
@@ -19,12 +19,153 @@ from gym_gomoku.envs.util import make_expert_policy
 # The winner is the first player to get an unbroken row of five stones horizontally, vertically, or diagonally. (so-calle five-in-a row)
 # Black plays first if white did not win in the previous game, and players alternate in placing a stone of their color on an empty intersection.
 
+
+
+class Board(object):
+    '''
+    Basic Implementation of a Go Board, natural action are int [0,board_size**2)
+    '''
+    def __init__(self, board_size: int):
+        self.size = board_size
+        self.board_state = [[Color.empty.value] * board_size for i in range(board_size)]  # initialize board states to empty
+        self.move = 0                 # how many move has been made
+        self.last_coord = (-1, -1)     # last action coord
+        self.last_action = None       # last action made
+        self.position_sets = {Color.empty: set(range(board_size**2)),
+                              Color.black: set(),
+                              Color.white: set()}
+
+    @property
+    def valid_actions(self):
+        return self.position_sets[Color.empty]
+
+    def coord_to_action(self, i, j):
+        ''' convert coordinate i, j to action a in [0, board_size**2)
+        '''
+        a = i * self.size + j  # action index
+        return a
+
+    def action_to_coord(self, a):
+        coord = (a // self.size, a % self.size)
+        return coord
+
+    def get_legal_move(self):
+        ''' Get all the next legal move, namely empty space that you can place your 'color' stone
+            Return: Coordinate of all the empty space, [(x1, y1), (x2, y2), ...]
+        '''
+        legal_move = []
+        for i in range(self.size):
+            for j in range(self.size):
+                if (self.board_state[i][j] == 0):
+                    legal_move.append((i, j))
+        return legal_move
+
+    def get_legal_action(self):
+        ''' Get all the next legal action, namely empty space that you can place your 'color' stone
+            Return: Coordinate of all the empty space, [(x1, y1), (x2, y2), ...]
+        '''
+        legal_action = []
+        for i in range(self.size):
+            for j in range(self.size):
+                if (self.board_state[i][j] == 0):
+                    legal_action.append(self.coord_to_action(i, j))
+        return legal_action
+
+    def copy(self, board_state, position_sets):
+        '''update board_state of current board values from input 2D list
+        '''
+        input_size_x = len(board_state)
+        input_size_y = len(board_state[0])
+        assert input_size_x == input_size_y, 'input board_state two axises size mismatch'
+        assert len(self.board_state) == input_size_x, 'input board_state size mismatch'
+        for i in range(self.size):
+            for j in range(self.size):
+                self.board_state[i][j] = board_state[i][j]
+
+        # copy the position_sets
+        self.position_sets = dict([(k, set(v)) for k, v in position_sets.items()])
+
+    def play(self, action, color):
+        '''
+            Args: input action, current player color
+            Return: new copy of board object
+        '''
+        b = Board(self.size)
+        b.copy(self.board_state, self.position_sets)  # create a board copy of current board_state
+        b.move = self.move
+
+        coord = self.action_to_coord(action)
+        # check if it's legal move
+        if b.board_state[coord[0]][coord[1]] != 0:  # the action coordinate is not empty
+            raise error.InvalidAction("Action is illegal, position [%d, %d] on board is not empty" % ((coord[0]+1),(coord[1]+1)))
+        # check if it's legal move
+        if action not in b.position_sets[Color.empty]:
+            raise error.InvalidAction("Action is illegal, position [%d, %d] on board is not empty" % ((coord[0] + 1), (coord[1] + 1)))
+
+        # make move
+        b.position_sets[Color.empty].remove(action)
+        b.position_sets[color].add(action)
+
+        b.board_state[coord[0]][coord[1]] = color.value
+        b.move += 1  # move counter add 1
+        b.last_coord = coord  # save last coordinate
+        b.last_action = action
+        return b
+
+    def is_terminal(self):
+        exist, color = gomoku_util.check_five_in_row(self.board_state)
+        is_full = gomoku_util.check_board_full(self.board_state)
+        if (is_full): # if the board if full of stones and no extra empty spaces, game is finished
+            return True
+        else:
+            return exist
+
+    def __repr__(self):
+        ''' representation of the board class
+            print out board_state
+        '''
+        out = ""
+        size = len(self.board_state)
+
+        letters = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')[:size]
+
+        label_move = "Move: " + str(self.move) + "\n"
+        label_letters = "     " + " ".join(letters) + "\n"
+        label_letters = "     " + " ".join(letters) + "\n"
+        label_boundry = "   " + "+-" + "".join(["-"] * (2 * size)) + "+" + "\n"
+
+        # construct the board output
+        out += (label_move + label_letters + label_boundry)
+
+        for i in range(size-1,-1,-1):
+            line = ""
+            line += (str("%2d" % (i+1)) + " |" + " ")
+            for j in range(size):
+                # check if it's the last move
+                line += Color(self.board_state[i][j]).shape
+                if (i, j) == self.last_coord:
+                    line += ")"
+                else:
+                    line += " "
+            line += ("|" + "\n")
+            out += line
+        out += (label_boundry + label_letters)
+        return out
+
+    def encode(self):
+        ''' Return: np array
+            np.array(board_size, board_size): state observation of the board
+        '''
+        img = np.array(self.board_state) # shape [board_size, board_size]
+        return img
+
+
 class GomokuState(object):
     '''
     Similar to Go game, Gomoku state consists of a current player and a board.
     Actions are exposed as integers in [0, num_actions), which is to place stone on empty intersection
     '''
-    def __init__(self, board, color: Color):
+    def __init__(self, board: Board, color: Color):
         '''
         Args:
             board: current board
@@ -41,6 +182,30 @@ class GomokuState(object):
             a new GomokuState with the new board and the player switched
         '''
         return GomokuState(self.board.play(action, self.color), self.color.other)
+
+    @property
+    def empty(self):
+        return self.board.position_sets[Color.empty]
+
+    @property
+    def mine(self):
+        return self.board.position_sets[self.color]
+
+    @property
+    def others(self):
+        return self.board.position_sets[self.color.other]
+
+    def coord_to_action(self, i, j=None) -> int:
+        ''' convert coordinate i, j to action a in [0, board_size**2)
+        '''
+        if j is None:
+            i, j = i
+        a = i * self.board.size + j  # action index
+        return a
+
+    def action_to_coord(self, a: int) -> Tuple[int, int]:
+        coord = (a // self.board.size, a % self.board.size)
+        return coord
 
     def __repr__(self):
         '''stream of board shape output'''
@@ -242,142 +407,3 @@ class GomokuEnv(gym.Env):
             self.opponent_policy = None
         else:
             raise error.Unregistered('Unrecognized opponent policy {}'.format(self.opponent))
-
-
-class Board(object):
-    '''
-    Basic Implementation of a Go Board, natural action are int [0,board_size**2)
-    '''
-    def __init__(self, board_size):
-        self.size = board_size
-        self.board_state = [[Color.empty.value] * board_size for i in range(board_size)]  # initialize board states to empty
-        self.move = 0                 # how many move has been made
-        self.last_coord = (-1, -1)     # last action coord
-        self.last_action = None       # last action made
-        self.position_sets = {Color.empty: set(range(board_size**2)),
-                              Color.black: set(),
-                              Color.white: set()}
-
-    @property
-    def valid_actions(self):
-        return self.position_sets[Color.empty]
-    
-    def coord_to_action(self, i, j):
-        ''' convert coordinate i, j to action a in [0, board_size**2)
-        '''
-        a = i * self.size + j  # action index
-        return a
-    
-    def action_to_coord(self, a):
-        coord = (a // self.size, a % self.size)
-        return coord
-    
-    def get_legal_move(self):
-        ''' Get all the next legal move, namely empty space that you can place your 'color' stone
-            Return: Coordinate of all the empty space, [(x1, y1), (x2, y2), ...]
-        '''
-        legal_move = []
-        for i in range(self.size):
-            for j in range(self.size):
-                if (self.board_state[i][j] == 0):
-                    legal_move.append((i, j))
-        return legal_move
-    
-    def get_legal_action(self):
-        ''' Get all the next legal action, namely empty space that you can place your 'color' stone
-            Return: Coordinate of all the empty space, [(x1, y1), (x2, y2), ...]
-        '''
-        legal_action = []
-        for i in range(self.size):
-            for j in range(self.size):
-                if (self.board_state[i][j] == 0):
-                    legal_action.append(self.coord_to_action(i, j))
-        return legal_action
-    
-    def copy(self, board_state, position_sets):
-        '''update board_state of current board values from input 2D list
-        '''
-        input_size_x = len(board_state)
-        input_size_y = len(board_state[0])
-        assert input_size_x == input_size_y, 'input board_state two axises size mismatch'
-        assert len(self.board_state) == input_size_x, 'input board_state size mismatch'
-        for i in range(self.size):
-            for j in range(self.size):
-                self.board_state[i][j] = board_state[i][j]
-
-        # copy the position_sets
-        self.position_sets = dict([(k, set(v)) for k, v in position_sets.items()])
-    
-    def play(self, action, color):
-        '''
-            Args: input action, current player color
-            Return: new copy of board object
-        '''
-        b = Board(self.size)
-        b.copy(self.board_state, self.position_sets)  # create a board copy of current board_state
-        b.move = self.move
-        
-        coord = self.action_to_coord(action)
-        # check if it's legal move
-        if b.board_state[coord[0]][coord[1]] != 0:  # the action coordinate is not empty
-            raise error.InvalidAction("Action is illegal, position [%d, %d] on board is not empty" % ((coord[0]+1),(coord[1]+1)))
-        # check if it's legal move
-        if action not in b.position_sets[Color.empty]:
-            raise error.InvalidAction("Action is illegal, position [%d, %d] on board is not empty" % ((coord[0] + 1), (coord[1] + 1)))
-
-        # make move
-        b.position_sets[Color.empty].remove(action)
-        b.position_sets[color].add(action)
-        
-        b.board_state[coord[0]][coord[1]] = color.value
-        b.move += 1  # move counter add 1
-        b.last_coord = coord  # save last coordinate
-        b.last_action = action
-        return b
-    
-    def is_terminal(self):
-        exist, color = gomoku_util.check_five_in_row(self.board_state)
-        is_full = gomoku_util.check_board_full(self.board_state)
-        if (is_full): # if the board if full of stones and no extra empty spaces, game is finished
-            return True
-        else:
-            return exist
-    
-    def __repr__(self):
-        ''' representation of the board class
-            print out board_state
-        '''
-        out = ""
-        size = len(self.board_state)
-        
-        letters = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')[:size]
-        
-        label_move = "Move: " + str(self.move) + "\n"
-        label_letters = "     " + " ".join(letters) + "\n"
-        label_letters = "     " + " ".join(letters) + "\n"
-        label_boundry = "   " + "+-" + "".join(["-"] * (2 * size)) + "+" + "\n"
-        
-        # construct the board output
-        out += (label_move + label_letters + label_boundry)
-        
-        for i in range(size-1,-1,-1):
-            line = ""
-            line += (str("%2d" % (i+1)) + " |" + " ")
-            for j in range(size):
-                # check if it's the last move
-                line += Color(self.board_state[i][j]).shape
-                if (i, j) == self.last_coord:
-                    line += ")"
-                else:
-                    line += " "
-            line += ("|" + "\n")
-            out += line
-        out += (label_boundry + label_letters)
-        return out
-    
-    def encode(self):
-        ''' Return: np array
-            np.array(board_size, board_size): state observation of the board
-        '''
-        img = np.array(self.board_state) # shape [board_size, board_size]
-        return img
